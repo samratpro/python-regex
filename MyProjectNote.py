9d822825cc90bed7e8673d39feeccf97cb9af101
Samrat Biswas <102436136+samratpro@users.noreply.github.com>
2023-05-22 03:50:41 +0600

    Update MyProjectNote.py

--- MyProjectNote.py
+++ MyProjectNote.py
@@ -7,3 +7,19 @@ pattern = r'([\w]+\.[\w]+)'
 replacement = r'<a href="\1">\1</a>'
 result = re.sub(pattern, replacement, text)
 # Output: The link is <a href="gran.com">gran.com</a>. The price <a href="issol.com">issol.com</a>
+
+
+text = "Additionally, there are.several job postings.available for Charge Solar in Canada on Indeed.com, indicating that they are an active and growing.company"
+converted_text = re.sub(r'(\b(?:are|several))\.(\w+\b)(?!\.com)', r'\1. \2', text)
+print(converted_text)
+# Output: Additionally, there are. several job postings.available for Charge Solar in Canada on Indeed.com, indicating that they are an active and growing.company 
+
+"""
+Certainly! Let's break down the modified regex pattern used in the code: (\b(?:are|several))\.(\w+\b)(?!\.com).
+(\b(?:are|several)) captures the words "are" or "several" as a group using a non-capturing group (?: ). The word boundary \b ensures that it matches the whole word and not a partial word.
+\. matches the dot character.
+(\w+\b) captures one or more word characters (letters, digits, or underscores) followed by a word boundary. This captures the word immediately after the dot.
+(?!\.com) is a negative lookahead assertion that ensures the pattern doesn't match if the dot is followed by ".com". The (?!\ ) construct is used to specify that the pattern inside the lookahead should not be present at that point.
+In the replacement pattern r'\1. \2', the \1 represents the first captured group, which is "are" or "several". The dot and space are added between \1 and \2, which is the second captured group, representing the word immediately after the dot.
+By using this modified pattern, we can add a space between "are." and "several" while preserving the ".com" part of the domain.
+"""
